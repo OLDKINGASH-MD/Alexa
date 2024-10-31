202460348f18626da036c1e5d8a35a7b5e98b2ea
OLDKINGASH-MD <lokuashmd@gmail.com>
2024-10-31 23:28:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "GfITAYxI#A_e8hFrjQZM8ElU3lwl-Wp-rYA_-rO1L0FLeubpvBow",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_SAVE_STATUS: process.env.AUTO_SAVE_STATUS || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 };
